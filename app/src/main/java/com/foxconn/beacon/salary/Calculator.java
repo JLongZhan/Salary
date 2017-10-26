e883639fb480c1bfb426f336795016a2de4bbeae
JLow <1138060860@qq.com>
2017-10-26 13:36:08 +0800

    add AndoridUnitTest

---
+++ app/src/main/java/com/foxconn/beacon/salary/Calculator.java
@@ -0,0 +1,24 @@
+package com.foxconn.beacon.salary;
+
+/**
+ * @author: F1331886
+ * @date: 2017/10/26 0026.
+ * @describe:
+ */
+
+public class Calculator {
+    public int sum(int a, int b) {
+        return a + b;
+    }
+    public int divide(int a, int b) {
+        return a / b;
+    }
+
+    public double multiply(double a, double b) {
+        return a * b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+}
